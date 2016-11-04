bb82ed06cce0c33e125e6fc76f3180ff049d2939
llancon <rdzalb@gmail.com>
2016-11-04 16:24:20 -0400

    Fixed heroku

--- app/controllers/following_controller.rb
+++ app/controllers/following_controller.rb
@@ -1,11 +1,11 @@
 class FollowingController < ApplicationController
 
 
-  acts_as_follower
-  acts_as_followable
-  acts_as_liker
-  acts_as_likeable
-  acts_as_mentionable
+  # acts_as_follower
+  # acts_as_followable
+  # acts_as_liker
+  # acts_as_likeable
+  # acts_as_mentionable
 
 
 
