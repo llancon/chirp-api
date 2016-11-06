985f94c107863b8501141be11b39496043cc3af3
llancon <rdzalb@gmail.com>
2016-11-06 12:40:14 -0500

    Adjust post to limit to 160 characters

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,8 @@ Rails.application.routes.draw do
   post '/follow' => 'users#follow_user'
   post '/unfollow' => 'users#unfollow_user'
   get '/allfollowers' => 'user#all_followers'
-  resources :users do
+  resources :users
   resources :posts
-  end
+  
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
