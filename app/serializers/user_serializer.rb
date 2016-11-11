57eee5b268c8954b6c396cb4890625c33919bb37
llancon <rdzalb@gmail.com>
2016-11-11 17:38:00 -0500

    Edited avatar_id

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,11 +1,11 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :username, :email, :created_at, :api_token, :avatar, :id, :followers_count, :followees_count
+  attributes :username, :email, :created_at, :api_token, :avatar, :followers_count, :followees_count
   has_many :followers
   has_many :posts
 
 
   def avatar
-    if avatar_id == null
+    if avatar == null
       'https://robohash.org/' + "#{object.username}"
     else
       Refile.attachment_url(object, :avatar, :fit, 50, 50, format: "jpg")
